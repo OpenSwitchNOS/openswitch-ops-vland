2cf8278b67e5179fcdee4212851ff0157aa68489
Swati Madime <swati.ran.madime@hpe.com>
2016-03-13 23:48:13 -0700

    Fix:usr:bug#1762:"Internal vlans should not be assigned to L2 ports as trunk or native." Fix:usr:bug#1406:"Ports needs to be renamed as interfaces in all VLAN CLI configuration and output"
    Change-Id: I50c42405336a15f47f34a58734dfffe2309cd0ac
    Signed-off-by: Swati Madime <swati.ran.madime@hpe.com>

--- src/cli/vlan_vty.c
+++ src/cli/vlan_vty.c
@@ -1234,6 +1234,14 @@ DEFUN(cli_intf_vlan_trunk_allowed,
             vty_out(vty, OVSDB_INTF_VLAN_TRUNK_ALLOWED_ERROR, vlan_id, VTY_NEWLINE);
             return CMD_SUCCESS;
         }
+          /* Check for internal vlan use. */
+        if (check_internal_vlan(vlan_id) == 0)
+        {
+            vty_out(vty, "Error : Vlan ID-%d is an internal vlan.%s",vlan_id, VTY_NEWLINE);
+            list = list->link;
+            continue;
+        }
+
 
         char *ifname = (char *) vty->index;
 
@@ -1558,6 +1566,7 @@ DEFUN(cli_intf_vlan_trunk_native,
     const struct ovsrec_port *port_row = NULL;
     const struct ovsrec_port *vlan_port_row = NULL;
     const struct ovsrec_interface *intf_row = NULL;
+     const struct ovsrec_interface * tmp_row = NULL;
     const struct ovsrec_vlan *vlan_row = NULL;
     struct ovsdb_idl_txn *status_txn = cli_do_config_start();
     enum ovsdb_idl_txn_status status;
@@ -1570,6 +1579,16 @@ DEFUN(cli_intf_vlan_trunk_native,
         cli_do_config_abort(status_txn);
         vty_out(vty, OVSDB_INTF_VLAN_TRUNK_NATIVE_ERROR,vlan_id, VTY_NEWLINE);
         return CMD_SUCCESS;
+    }
+
+     /* Check for internal vlan use. */
+    OVSREC_INTERFACE_FOR_EACH(tmp_row, idl) {
+        if (check_internal_vlan(vlan_id) == 0)
+        {
+            vty_out(vty, "Error : Vlan ID-%d is an internal vlan.%s",vlan_id, VTY_NEWLINE);
+            cli_do_config_abort(status_txn);
+            return CMD_SUCCESS;
+        }
     }
 
     char *ifname = (char *) vty->index;
@@ -2722,7 +2741,7 @@ DEFUN(cli_show_vlan,
 
     vty_out(vty, "%s", VTY_NEWLINE);
     vty_out(vty, "--------------------------------------------------------------------------------------%s", VTY_NEWLINE);
-    vty_out(vty, "VLAN    Name            Status   Reason         Reserved       Ports%s", VTY_NEWLINE);
+    vty_out(vty, "VLAN    Name            Status   Reason         Reserved       Interfaces%s", VTY_NEWLINE);
     vty_out(vty, "--------------------------------------------------------------------------------------%s", VTY_NEWLINE);
 
     shash_init(&sorted_vlan_id);
@@ -2838,7 +2857,7 @@ DEFUN(cli_show_vlan_id,
 
     vty_out(vty, "%s", VTY_NEWLINE);
     vty_out(vty, "--------------------------------------------------------------------------------------%s", VTY_NEWLINE);
-    vty_out(vty, "VLAN    Name            Status   Reason         Reserved       Ports%s", VTY_NEWLINE);
+    vty_out(vty, "VLAN    Name            Status   Reason         Reserved       Interfaces%s", VTY_NEWLINE);
     vty_out(vty, "--------------------------------------------------------------------------------------%s", VTY_NEWLINE);
 
 
